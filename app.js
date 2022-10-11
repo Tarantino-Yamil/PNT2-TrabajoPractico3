6d80e60e718f191a6688faffcfdeb420a73fa757
Yamil Tarantino <yamiltarantino@gmail.com>
2022-10-11 15:04:28 -0300

    Cambios

---
+++ app.js
@@ -0,0 +1,111 @@
+new Vue({
+    el: '#app',
+    data: {
+        saludJugador: 100,
+        saludMonstruo: 100,
+        hayUnaPartidaEnJuego: false,
+        turnos: [], //es para registrar los eventos de la partida
+        esJugador: false,
+        rangoAtaque: [3, 10],
+        rangoAtaqueEspecial: [10, 20],
+        rangoAtaqueDelMonstruo: [5, 12],
+    },
+
+    methods: {
+        getSalud(salud) {
+            return `${salud}%`
+        },
+        empezarPartida: function () {
+            this.hayUnaPartidaEnJuego = true;
+            this.saludJugador = 100;
+            this.saludMonstruo = 100;
+            this.turnos = []
+        },
+        atacar: function () {
+            var daño = this.calcularHeridas(this.rangoAtaque);
+            this.saludMonstruo -= daño;
+            this.turnos.unshift({
+                esJugador: true,
+                text: 'El jugador golpea al monstruo por ' + daño
+            });
+            if(this.verificarGanador()){
+                return;
+            }
+            this.ataqueDelMonstruo();
+        },
+
+        ataqueEspecial: function () {
+            var daño = this.calcularHeridas(this.rangoAtaqueEspecial);
+            this.saludMonstruo -= daño;
+            this.turnos.unshift({
+                esJugador: true,
+                text: 'El jugador golpea al monstruo por ' + daño
+            });
+            if(this.verificarGanador()){
+                return;
+            }
+            this.ataqueDelMonstruo();
+        },
+
+        curar: function () {
+            if(this.saludJugador <= 90){
+                this.saludJugador += 10;
+                this.turnos.unshift({
+                    esJugador: true,
+                    text: 'El jugador se cura por ' + 10
+                });
+            } else {
+                this.saludJugador = 100;
+                }
+            this.ataqueDelMonstruo();
+        },
+
+        registrarEvento(evento) {
+        },
+        
+        terminarPartida: function () {
+            this.hayUnaPartidaEnJuego = false;  
+        },
+
+        ataqueDelMonstruo: function () {
+            var daño = this.calcularHeridas(this.rangoAtaqueDelMonstruo);
+            this.saludJugador -= daño;
+            this.turnos.unshift({
+                esJugador: false,
+                text: 'El monstruo golpea al jugador por ' + daño
+            });
+            this.verificarGanador();
+        },
+
+        calcularHeridas: function (rango) {
+            return Math.max(Math.floor(Math.random() * rango[1]) + 1, rango[0])
+
+        },
+        verificarGanador: function () {
+            if(this.saludMonstruo <= 0){
+                if(confirm('Ganaste! Desea jugar de nuevo?')){
+                    this.empezarPartida();
+                } else {
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            }
+            else if(this.saludJugador <= 0){
+                if(confirm('Perdiste! Desea jugar de nuevo?')){
+                    this.empezarPartida();
+                } else {
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            }
+            return false;
+        },
+        cssEvento(turno) {
+            //Este return de un objeto es prque vue asi lo requiere, pero ponerlo acá queda mucho mas entendible en el codigo HTML.
+            return {
+                'player-turno': turno.esJugador,
+                'monster-turno': !turno.esJugador
+            }
+        }
+    }
+});
